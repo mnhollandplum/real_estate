d90a1434cec601cdda9a5dc8e930ad8d8e2a7caf
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-08-29 13:50:39 -0600

    completes iteration 3

--- test/house_test.rb
+++ test/house_test.rb
@@ -36,6 +36,36 @@ class HouseTest < Minitest::Test
     assert_equal [room_1, room_2], house.rooms
   end
 
+  def test_rooms_can_be_determined_by_category_in_house
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
 
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal [room_1, room_2], house.rooms_from_category(:bedroom)
+
+    assert_equal [room_4], house.rooms_from_category(:basement)
+  end
+
+  def test_can_calculate_the_floor_area_for_house_based_on_added_rooms
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal 1900, house.area
+  end
 
 end
