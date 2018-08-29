ca812f1196ce5e6cb4b97009f8207856ca509084
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-08-29 14:32:51 -0600

    compeletes iteration 4

--- test/house_test.rb
+++ test/house_test.rb
@@ -36,7 +36,7 @@ class HouseTest < Minitest::Test
     assert_equal [room_1, room_2], house.rooms
   end
 
-  def test_rooms_can_be_determined_by_category_in_house
+  def test_can_list_rooms_based_on_category
     house = House.new("$400000", "123 sugar lane")
     room_1 = Room.new(:bedroom, 10, 13)
     room_2 = Room.new(:bedroom, 11, 15)
@@ -67,5 +67,52 @@ class HouseTest < Minitest::Test
 
     assert_equal 1900, house.area
   end
+
+  def test_can_calculate_price_per_square_foot_of_house
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal 210.53, house.price_per_square_foot
+  end
+
+  def test_can_sort_rooms_in_house_by_floor_area
+    house = House.new("$400000", "123 sugar lane")
+    room_3 = Room.new(:bedroom, 10, 13)
+    room_1 = Room.new(:bedroom, 11, 15)
+    room_4 = Room.new(:living_room, 25, 15)
+    room_2 = Room.new(:basement, 30, 41)
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal [room_3, room_1, room_4, room_2], house.rooms_sorted_by_area
+  end
+
+  def test_can_organize_rooms_by_category_in_hash
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal ({:bedroom=> [room_1, room_2], :living_room=> [room_3], :basement=> [room_4]}), house.rooms_by_category
+
+  end
+
 
 end
